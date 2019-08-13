4774f033939f6a3cdbf7a38e2ac6fa629d58a26b
Sergey <andryuhyn@gmail.com>
2019-08-13 17:49:16 +0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -45,6 +45,9 @@ var Core = new function () {
     var velocity = {x: 0, y: 0};
     var score = 0;
 
+    var playing = false;
+    var time = 0;
+    var duration = 0;
     ////////////////////////////
     var enemyCount = 0;
     var enemyBurnCount = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
@@ -55,6 +58,7 @@ var Core = new function () {
     var bullet = {x: 0, y: 0, dg: 0, dGipotenusa: 0, animation: false,};
     var aimX, lineX, seaBackground, skyBackgroundHeight, seaBackgroundHeight, alphaDeg, alphaDegConst, h, gipotenusa,
         alpha, bulletAnimationConst;
+    var enemyBulletStatus = false;
     var relativeX = 0;
     var relX = 0;
     var fire = false;
@@ -63,14 +67,17 @@ var Core = new function () {
     var ballRadius = 5;
     var skys = [];
     var submarinePadding = 20;
+    var submarineHeight = 50;
     var bubbles = [];
     var explosion = [];
 
     var newSky;
     var skyW = 1765;
+    var life = 100;
 
     this.init = function () {
         canvas = document.getElementById('world');
+        startButton = document.getElementById('startButton');
         if (canvas && canvas.getContext) {
             context = canvas.getContext('2d');
             canvas.width = DEFAULT_WIDTH;
@@ -94,6 +101,11 @@ var Core = new function () {
         }
     };
 
+    function gameOver() {
+        playing = false;
+        duration = new Date().getTime() - time;
+    }
+
     function mouseMoveHandler(event) {
         relativeX = event.clientX - canvas.offsetLeft;
         if (relativeX > 0 && relativeX < canvas.width) {
@@ -112,20 +124,36 @@ var Core = new function () {
     }
 
     function mouseClickHandler(event) {
-        fire = true;
-        bulletAnimationConst = (function () {
-            return bullet.animation;
-        })();
-        if (!bulletAnimationConst) {
-            bullet.x = canvas.width / 2;
-            bullet.dGipotenusa = 0;
-            bulletCount++;
-            alphaDegConst = (function () {
-                return alpha;
+        if (event.target == startButton) {
+            if( playing == false ) {
+                playing = true;
+                life = 100;
+                ships = [];
+                enemyCount = 0;
+                enemyBurnCount = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
+                hitStat = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
+                enemyBurnCountFull = 0;
+                enemyBurnCountFullConst;
+                bulletCount = 0;
+                enemyBulletStatus = false;
+                time = new Date().getTime();
+            }
+        } else {
+              fire = true;
+            bulletAnimationConst = (function () {
+                return bullet.animation;
             })();
-            enemyBurnCountFullConst = (function () {
-                return enemyBurnCountFull;
-            })();
+            if (!bulletAnimationConst) {
+                bullet.x = canvas.width / 2;
+                bullet.dGipotenusa = 0;
+                bulletCount++;
+                alphaDegConst = (function () {
+                    return alpha;
+                })();
+                enemyBurnCountFullConst = (function () {
+                    return enemyBurnCountFull;
+                })();
+            }          
         }
         //console.log(bulletAnimationConst);
     }
@@ -204,6 +232,14 @@ var Core = new function () {
         context.closePath();
     }
 
+    function drawEnemyBullet(x, y) {
+        context.beginPath();
+        context.arc(x, y, ballRadius, 0, Math.PI * 2);
+        context.fillStyle = "red";
+        context.fill();
+        context.closePath();
+    }
+
     function emitBubbles(position, direction, spread, seed) {
         var q = seed + (Math.random() * seed);
         while (--q >= 0) {
@@ -234,97 +270,114 @@ var Core = new function () {
         drawSeaBackground();
         drawSun();
 
-        for (i = 0; i < skys.length; i++) {
-            s = skys[i];
-            s.position.x += 0.25;
-            drawSky(s.position.x, s.position.y);
-            if (s.position.x == canvas.width) {
-                skys.splice(i, 1);
-                i--;
-            }
-        }
-        if (skys.length < 1) {
-            newSky = giveSky(new Sky());
-            skys.push(newSky);
-        } else if (skys.length > 0 && skys.length < 2) {
-            newSky = giveSky(new Sky());
-            newSky.position.x = canvas.width - ((skys.length + 1) * skyW);
-            skys.push(newSky);
-        }
+        if( playing ) {
 
-
-        h = (canvas.width / 2 - lineX);
-        gipotenusa = Math.sqrt(Math.pow(h, 2) + Math.pow(seaBackgroundHeight - submarinePadding, 2));
-        alpha = Math.atan(h / (seaBackgroundHeight - submarinePadding));
-        alphaDeg = alpha * 180 / Math.PI;
-
-        if (fire == true) {
-            bullet.animation = true;
-            var bbb = 0;
-            drawBullet(bullet.x, bullet.y);
-            drawSky(s.position.x, s.position.y);
-            bullet.dGipotenusa += bullet.dg;
-            bullet.x = canvas.width / 2 - (bullet.dGipotenusa * Math.sin(alphaDegConst));
-            bullet.y = (canvas.height - submarinePadding) - (bullet.dGipotenusa * Math.cos(alphaDegConst));
-        }
-
-        // Go through each enemy and draw it + update its properties
-        for (i = 0; i < ships.length; i++) {
-
-            p = ships[i];
-            if (p.status == 'live') {
-                p.position.x += p.velocity.x;
-                p.position.y = skyBackgroundHeight;
-            }
-            context.beginPath();
-            //context.rect(p.position.x, p.position.y, p.width, p.height);
-            context.drawImage(boatImg(p.size, p.status), p.position.x, p.position.y - p.draft);
-
-            if (p.position.x > canvas.width || p.position.y > canvas.height) {
-                p.status = 'dead';
-            }
-            ///////////////////////////
-            if (p.status == 'live' && bullet.x > p.position.x - bullet.deflection && bullet.x < p.position.x + p.width + bullet.deflection && bullet.y < skyBackgroundHeight + p.height) {
-                p.status = 'burn';
-                p.timeDeath = new Date().getTime();
-                enemyBurnSizeCount(p.size);
-                score += p.score; 
-                ++enemyBurnCountFull;
-            }
-            if (p.status == 'burn' || p.status == 'sink') {
-                if (p.status == 'burn') {
-                    emitFire({x: p.position.x + p.width / 2, y: p.position.y}, {x: 30 * 0.03, y: -30 * 0.01}, 1, p.size * 10);
-                    p.position.x += 0;
-                    p.position.y = p.position.y;
-                    p.y += p.velocity.y / 2;
+            for (i = 0; i < skys.length; i++) {
+                s = skys[i];
+                s.position.x += 0.25;
+                drawSky(s.position.x, s.position.y);
+                if (s.position.x == canvas.width) {
+                    skys.splice(i, 1);
+                    i--;
                 }
-                
-                if (new Date().getTime() - p.timeDeath > 200) {
-                  p.status = 'sink';  
+            }
+            if (skys.length < 1) {
+                newSky = giveSky(new Sky());
+                skys.push(newSky);
+            } else if (skys.length > 0 && skys.length < 2) {
+                newSky = giveSky(new Sky());
+                newSky.position.x = canvas.width - ((skys.length + 1) * skyW);
+                skys.push(newSky);
+            }
+
+
+            h = (canvas.width / 2 - lineX);
+            gipotenusa = Math.sqrt(Math.pow(h, 2) + Math.pow(seaBackgroundHeight - submarinePadding, 2));
+            alpha = Math.atan(h / (seaBackgroundHeight - submarinePadding));
+            alphaDeg = alpha * 180 / Math.PI;
+
+            if (fire == true) {
+                bullet.animation = true;
+                var bbb = 0;
+                drawBullet(bullet.x, bullet.y);
+                drawSky(s.position.x, s.position.y);
+                bullet.dGipotenusa += bullet.dg;
+                bullet.x = canvas.width / 2 - (bullet.dGipotenusa * Math.sin(alphaDegConst));
+                bullet.y = (canvas.height - submarinePadding) - (bullet.dGipotenusa * Math.cos(alphaDegConst));
+            }
+
+            // Go through each enemy and draw it + update its properties
+            for (i = 0; i < ships.length; i++) {
+
+                p = ships[i];
+                if (p.status == 'live') {
+                    p.position.x += p.velocity.x;
+                    p.position.y = skyBackgroundHeight;
                 }
-                if (p.status == 'sink') {
-                    p.position.x += 0;
-                    p.position.y += p.velocity.y / 2;
-                    var seeds = p.size - (p.size * (p.position.y - skyBackgroundHeight) / seaBackgroundHeight * 2);
-                    if (seeds > 0) {
-                        emitBubbles({x: p.position.x + p.width / 10 * 4, y: p.position.y}, {x: 40 * 0.03, y: -40 * 0.01}, 2, seeds);
+                context.beginPath();
+                //context.rect(p.position.x, p.position.y, p.width, p.height);
+                context.drawImage(boatImg(p.size, p.status), p.position.x, p.position.y - p.draft);
+
+                if (p.position.x > canvas.width/2) {
+                    drawEnemyBullet(p.enemyBullet.x, p.enemyBullet.y); 
+                    p.enemyBullet.x = canvas.width/2; 
+                    p.enemyBullet.y += p.enemyBullet.dy;
+                    if (p.enemyBullet.y > canvas.height - submarineHeight && p.enemyBullet.y < canvas.height) {
+                        p.enemyBullet.y = p.enemyBullet.y;
+                        enemyBulletStatus = true;
+                    } else if (p.enemyBullet.y > canvas.height) {
+                        enemyBulletStatus = false; 
                     }
                 }
+
+
+                if (p.position.x > canvas.width || p.position.y > canvas.height) {
+                    p.status = 'dead';
+                }
+                ///////////////////////////
+                if (p.status == 'live' && bullet.x > p.position.x - bullet.deflection && bullet.x < p.position.x + p.width + bullet.deflection && bullet.y < skyBackgroundHeight + p.height) {
+                    p.status = 'burn';
+                    p.timeDeath = new Date().getTime();
+                    enemyBurnSizeCount(p.size);
+                    score += p.score; 
+                    ++enemyBurnCountFull;
+                }
+                if (p.status == 'burn' || p.status == 'sink') {
+                    if (p.status == 'burn') {
+                        emitFire({x: p.position.x + p.width / 2, y: p.position.y}, {x: 30 * 0.03, y: -30 * 0.01}, 1, p.size * 10);
+                        p.position.x += 0;
+                        p.position.y = p.position.y;
+                        p.y += p.velocity.y / 2;
+                    }
+                    
+                    if (new Date().getTime() - p.timeDeath > 200) {
+                      p.status = 'sink';  
+                    }
+                    if (p.status == 'sink') {
+                        p.position.x += 0;
+                        p.position.y += p.velocity.y / 2;
+                        var seeds = p.size - (p.size * (p.position.y - skyBackgroundHeight) / seaBackgroundHeight * 2);
+                        if (seeds > 0) {
+                            emitBubbles({x: p.position.x + p.width / 10 * 4, y: p.position.y}, {x: 40 * 0.03, y: -40 * 0.01}, 2, seeds);
+                        }
+                    }
+                }
+
+                if (p.status == 'dead') {
+                    ships.splice(i, 1);
+                    i--;
+                }
+
+                context.fill();
+
             }
 
-            if (p.status == 'dead') {
-                ships.splice(i, 1);
-                i--;
-            }
-
-            context.fill();
-
-        }
         // If there are less enemies than intended for this difficulty, add another one
-
+        drawSeaBackground2();
         if (new Date().getTime() - lastspawn > 100) {
             var newEnemy = giveLife(new Enemy());
             //console.log(newEnemy);
+            newEnemy.enemyBullet.y = skyBackgroundHeight;
             ships.push(newEnemy);
             enemyCount++;
             var delay = randomInteger(SHIP_DELAY_MIN, SHIP_DELAY_MAX);
@@ -380,7 +433,16 @@ var Core = new function () {
             bullet.animation = false;
         }
 
-        drawSeaBackground2();
+        //drawSeaBackground2();
+        if (enemyBulletStatus == true) {
+            emitBubbles({x: canvas.width/2, y: canvas.height - submarineHeight}, {x: 30 * 0.03, y: -30 * 0.01}, 2, 2);
+            life = life - 0.5;
+        }
+    }
+        if (life < 0) {
+            gameOver();
+            life = 0;
+        }
         drawText('Всего врагов: ' + enemyCount, 10, canvas.height - 190, 18);
         drawText(enemyBurnTextCount()[0], 10, canvas.height - 160, 18);
         drawText(enemyBurnTextCount()[1], 10, canvas.height - 140, 12);
@@ -390,6 +452,7 @@ var Core = new function () {
         drawText(enemyBurnTextCount()[5], 10, canvas.height - 60, 12);
         drawText('Использовано торпед: ' + bulletCount, 10, canvas.height - 30, 18);
         drawAim();
+        drawText('Жизнь: ' + life + '%', canvas.width - 150, canvas.height - 150, 18);
         drawText('Промахов:  ' + hitStat[0], canvas.width - 150, canvas.height - 120, 12);
         drawText('Попаданий: ' + hitStat[1], canvas.width - 150, canvas.height - 100, 12);
         drawText('Комбо: ' + hitStat[2], canvas.width - 150, canvas.height - 80, 12);
@@ -502,6 +565,7 @@ function Enemy() {
     this.draft = enemySize[this.size].draft;
     this.timeDeath = null;
     this.score = enemySize[this.size].score;
+    this.enemyBullet = {x: 0, y: 0, dy: 2, status: false};
 }
 
 function Sky() {
